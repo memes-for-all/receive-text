7f578be85d3d26d66246171d8d77aa3067f8cdb1
Josh Martin <joshmartin5001@gmail.com>
2019-02-16 23:38:50 -0600

    Starting to work

--- index.js
+++ index.js
@@ -8,6 +8,19 @@ const MessagingResponse = twilio.twiml.MessagingResponse;
 const projectId = process.env.GCLOUD_PROJECT;
 const region = 'us-central1';
 
+const output_message = (name, nameOfServe, hoursTilMeme) => (
+    `Hello ${name},
+Welcome to ${nameOfServe}. Your next meme will drop in ${hoursTilMeme} hrs.
+For a list of actions type '/list'
+- an intellectual  
+`
+)
+const regeration_message = (name) => (
+`Hello ${name},
+you seem not to be an intellectual. Ask an intellectual how to subscribe...
+- an intellectual`
+)
+
 exports.reply = (req, res) => {
   let isValid = true;
     console.log(req.body);
@@ -31,9 +44,14 @@ exports.reply = (req, res) => {
 
   // Prepare a response to the SMS message
   const response = new MessagingResponse();
-
+  const userContents = req.body;
+  const userMessage = userContents.Body;
   // Add text to the response
-  response.message(req.body.Body);
+  if (userMessage.toLowerCase() === 'send dank memes')
+    response.message(output_message(userContents.From, "Meme for All", 9));
+    else {
+    response.message(regeration_message(userContents.From));
+    }
 
   // Send the response
   res
